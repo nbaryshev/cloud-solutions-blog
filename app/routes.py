70a6d6a53ce9765d486f3d7789563bec38df8ce7
Nikita Baryshev <nbasharkin@gmail.com>
2020-02-22 16:10:19 +0200

    when you click the post it opens on another page

--- app/routes.py
+++ app/routes.py
@@ -13,17 +13,17 @@ def homepage():
 def history():
     history_posts = models.Post.query.filter_by(topic='History').all()
 
-    return flask.render_template('history.jin', posts = history_posts)
+    return flask.render_template('history.jin', posts=history_posts)
 
 
 @app.route('/building')
 def building():
     building_posts = models.Post.query.filter_by(topic='Building').all()
 
-    return flask.render_template('building.jin', posts = building_posts)
+    return flask.render_template('building.jin', posts=building_posts)
 
 
-@app.route('/new-post', methods = ('GET', 'POST'))
+@app.route('/new-post', methods=('GET', 'POST'))
 def newpost():
     post = forms.NewPost()
 
@@ -40,3 +40,10 @@ def newpost():
             return flask.render_template('newpost.jin', form=post)
 
     return flask.render_template('newpost.jin', form=post)
+
+
+@app.route('/history/<int:post_id>')
+def post(post_id):
+    current_post = models.Post.query.filter_by(id=post_id).first()
+
+    return flask.render_template('post.jin', post=current_post)
