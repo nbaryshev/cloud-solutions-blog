90e51b8f5a51b996a4123590ad4b7a7395c16836
Nikita Baryshev <nbasharkin@gmail.com>
2020-05-04 19:41:41 +0300

    heroku deployment TEST

--- app/__init__.py
+++ app/__init__.py
@@ -7,25 +7,30 @@ from flask_uploads import configure_uploads, IMAGES, UploadSet
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# if "ONHEROKU" in os.environ:
-#     on_heroku = True
-#
-# else:
-#     on_heroku = False
+if "ON_HEROKU" in os.environ:
+    on_heroku = True
+else:
+    on_heroku = False
 
 app = flask.Flask(__name__)
 
 app.config['SECRET_KEY'] = 'sdjhgsjghlakjf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, "app.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, "app.db")
+
+# sqlite:////Users/nikita/Desktop/DevInst/Heroku/My_blog/app/app.db
+
 
 app.config['UPLOADED_IMAGES_DEST'] = os.path.join(basedir, 'static/images')
 images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
 
-# if on_heroku:
-#     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-# else:
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/myblog'
+if on_heroku:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    print("Test1")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL_LOCAL']
+    print("Test2")
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/d9tn41u3b8usfs'
 
 
 db = flask_sqlalchemy.SQLAlchemy(app)
